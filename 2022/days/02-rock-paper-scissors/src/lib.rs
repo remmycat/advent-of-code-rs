89d6f9ab916f8ab37acc36ef81aa6182d8fcf1f9
Remmy Cat Stock <3317423+remmycat@users.noreply.github.com>
2022-12-03 00:13:44 +0100

    why not overengineer it to let the compiler do the math instead

--- 2022/days/02-rock-paper-scissors/src/lib.rs
+++ 2022/days/02-rock-paper-scissors/src/lib.rs
@@ -13,41 +13,110 @@ const SCISSORS_X: &[u8] = b"C X\n";
 const SCISSORS_Y: &[u8] = b"C Y\n";
 const SCISSORS_Z: &[u8] = b"C Z\n";
 
-mod scores {
-	pub const ROCK: u64 = 1;
-	pub const PAPER: u64 = 2;
-	pub const SCISSORS: u64 = 3;
+enum GameResult {
+	Loss,
+	Draw,
+	Win,
+}
 
-	pub const LOSS: u64 = 0;
-	pub const DRAW: u64 = 3;
-	pub const WIN: u64 = 6;
+impl GameResult {
+	const fn score(&self) -> u64 {
+		match self {
+			GameResult::Loss => 0,
+			GameResult::Draw => 3,
+			GameResult::Win => 6,
+		}
+	}
+}
 
-	pub const ROCK_ROCK: u64 = ROCK + DRAW;
-	pub const ROCK_PAPER: u64 = PAPER + WIN;
-	pub const ROCK_SCISSORS: u64 = SCISSORS + LOSS;
-	pub const PAPER_ROCK: u64 = ROCK + LOSS;
-	pub const PAPER_PAPER: u64 = PAPER + DRAW;
-	pub const PAPER_SCISSORS: u64 = SCISSORS + WIN;
-	pub const SCISSORS_ROCK: u64 = ROCK + WIN;
-	pub const SCISSORS_PAPER: u64 = PAPER + LOSS;
-	pub const SCISSORS_SCISSORS: u64 = SCISSORS + DRAW;
+#[derive(Clone, Copy)]
+enum Shape {
+	Rock,
+	Paper,
+	Scissors,
+}
+
+impl Shape {
+	const fn score(&self) -> u64 {
+		use Shape::{Paper, Rock, Scissors};
+		match self {
+			Rock => 1,
+			Paper => 2,
+			Scissors => 3,
+		}
+	}
+
+	const fn get_result(self, opposing_shape: Shape) -> GameResult {
+		use GameResult::{Draw, Loss, Win};
+		use Shape::{Paper, Rock, Scissors};
+		match self {
+			Rock => match opposing_shape {
+				Rock => Draw,
+				Paper => Loss,
+				Scissors => Win,
+			},
+			Paper => match opposing_shape {
+				Rock => Win,
+				Paper => Draw,
+				Scissors => Loss,
+			},
+			Scissors => match opposing_shape {
+				Rock => Loss,
+				Paper => Win,
+				Scissors => Draw,
+			},
+		}
+	}
+
+	const fn find_response_for_outcome(self, outcome: GameResult) -> Self {
+		use GameResult::{Draw, Loss, Win};
+		use Shape::{Paper, Rock, Scissors};
+		match outcome {
+			Draw => self,
+			Loss => match self {
+				Rock => Scissors,
+				Paper => Rock,
+				Scissors => Paper,
+			},
+			Win => match self {
+				Rock => Paper,
+				Paper => Scissors,
+				Scissors => Rock,
+			},
+		}
+	}
+}
+
+const fn get_wrong_score(theirs: Shape, ours: Shape) -> u64 {
+	ours.score() + ours.get_result(theirs).score()
+}
+
+const fn get_correct_score(theirs: Shape, outcome: GameResult) -> u64 {
+	outcome.score() + theirs.find_response_for_outcome(outcome).score()
+}
+
+const fn get_both_scores(theirs: Shape, ours: Shape, or_result: GameResult) -> (u64, u64) {
+	(
+		get_wrong_score(theirs, ours),
+		get_correct_score(theirs, or_result),
+	)
 }
 
 fn get_round_scores(line: &[u8]) -> (u64, u64) {
-	use scores::*;
+	use GameResult::{Draw, Loss, Win};
+	use Shape::{Paper, Rock, Scissors};
 
 	#[allow(clippy::identity_op)]
 	match line {
-		ROCK_X => (ROCK_ROCK, ROCK_SCISSORS),
-		ROCK_Y => (ROCK_PAPER, ROCK_ROCK),
-		ROCK_Z => (ROCK_SCISSORS, ROCK_PAPER),
-		PAPER_X => (PAPER_ROCK, PAPER_ROCK),
-		PAPER_Y => (PAPER_PAPER, PAPER_PAPER),
-		PAPER_Z => (PAPER_SCISSORS, PAPER_SCISSORS),
-		SCISSORS_X => (SCISSORS_ROCK, SCISSORS_PAPER),
-		SCISSORS_Y => (SCISSORS_PAPER, SCISSORS_SCISSORS),
-		SCISSORS_Z => (SCISSORS_SCISSORS, SCISSORS_ROCK),
-		[] => (0, 0),
+		ROCK_X => get_both_scores(Rock, Rock, Loss),
+		ROCK_Y => get_both_scores(Rock, Paper, Draw),
+		ROCK_Z => get_both_scores(Rock, Scissors, Win),
+		PAPER_X => get_both_scores(Paper, Rock, Loss),
+		PAPER_Y => get_both_scores(Paper, Paper, Draw),
+		PAPER_Z => get_both_scores(Paper, Scissors, Win),
+		SCISSORS_X => get_both_scores(Scissors, Rock, Loss),
+		SCISSORS_Y => get_both_scores(Scissors, Paper, Draw),
+		SCISSORS_Z => get_both_scores(Scissors, Scissors, Win),
 		_ => panic!("malformed input"),
 	}
 }
