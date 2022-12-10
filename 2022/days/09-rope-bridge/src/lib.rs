811b203daca0bbf02d48f8069c597b7a386556bd
Remmy Cat Stock <3317423+remmycat@users.noreply.github.com>
2022-12-10 22:23:35 +0100

    Damn you, 2d grids of unknown size. Got it somewhat fast I guess.

--- 2022/days/09-rope-bridge/src/lib.rs
+++ 2022/days/09-rope-bridge/src/lib.rs
@@ -1,7 +1,4 @@
-use std::{
-	collections::HashSet,
-	ops::{Add, AddAssign, Sub},
-};
+use std::ops::{Add, AddAssign, Sub};
 
 #[derive(Debug, PartialEq, Eq)]
 pub struct Solution(u64, u64);
@@ -66,6 +63,73 @@ impl Sub for Point {
 	}
 }
 
+impl Ord for Point {
+	fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+		self.0.cmp(&other.0).then(self.1.cmp(&other.1))
+	}
+}
+
+impl PartialOrd for Point {
+	fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+		Some(self.cmp(other))
+	}
+}
+
+const GRID_SIZE: usize = 128;
+const GRID_DIV: assumptions::GridInt = GRID_SIZE as assumptions::GridInt;
+
+/// Stores a 2d grid of bool values as binary inside `u128`s.
+/// These are grouped in "chunks", each covering a 128x128 square of the grid.
+#[derive(Debug)]
+struct CondensedBooleanGrid {
+	chunks: Vec<(Point, [u128; GRID_SIZE])>,
+}
+
+impl CondensedBooleanGrid {
+	fn new() -> Self {
+		Self { chunks: vec![] }
+	}
+
+	fn get_chunk_index(p: Point) -> Point {
+		// the -1 for negative is to have only one chunk be responsible for x=0 / y=0
+		let x_div = p.0 / GRID_DIV - p.0.is_negative() as assumptions::GridInt;
+		let y_div = p.1 / GRID_DIV - p.1.is_negative() as assumptions::GridInt;
+		Point(x_div, y_div)
+	}
+
+	fn get_local_coords(p: Point) -> (u128, usize) {
+		let x_local = (p.0 % GRID_DIV).unsigned_abs() as u128;
+		let y_local = (p.1 % GRID_DIV).unsigned_abs() as usize;
+
+		(x_local, y_local)
+	}
+
+	fn get_chunk(&mut self, index: &Point) -> &mut [u128; GRID_SIZE] {
+		match self.chunks.binary_search_by_key(&index, |(p, _)| p) {
+			Ok(pos) => &mut self.chunks.get_mut(pos).expect("impossible").1,
+			Err(pos) => {
+				self.chunks.insert(pos, (index.to_owned(), [0; GRID_SIZE]));
+				&mut self.chunks.get_mut(pos).expect("impossible").1
+			}
+		}
+	}
+
+	fn set_true(&mut self, point: Point) {
+		let chunk_index = CondensedBooleanGrid::get_chunk_index(point);
+		let chunk = self.get_chunk(&chunk_index);
+
+		let (x, y) = CondensedBooleanGrid::get_local_coords(point);
+		chunk[y] |= 1_u128 << x;
+	}
+
+	fn count(&self) -> usize {
+		self.chunks
+			.iter()
+			.flat_map(|(_, chunk)| chunk.iter().map(|e| e.count_ones() as usize))
+			.sum()
+	}
+}
+
 const NUMBERS_START: u8 = b'0';
 fn parse_usize(b: &[u8]) -> usize {
 	let mut num = (b[0] - NUMBERS_START) as usize;
@@ -83,8 +147,11 @@ const START_POINT: Point = Point(0, 0);
 pub fn solve(input: &[u8]) -> Solution {
 	let mut head = START_POINT;
 	let mut rope = [START_POINT; 9];
-	let mut tail_visited: Vec<Point> = vec![START_POINT];
-	let mut long_tail_visited: Vec<Point> = vec![START_POINT];
+	let mut tail_visited: CondensedBooleanGrid = CondensedBooleanGrid::new();
+	let mut long_tail_visited: CondensedBooleanGrid = CondensedBooleanGrid::new();
+
+	tail_visited.set_true(START_POINT);
+	long_tail_visited.set_true(START_POINT);
 
 	for line in input.split(|b| *b == b'\n') {
 		let dir: Point = line[0].into();
@@ -106,10 +173,10 @@ pub fn solve(input: &[u8]) -> Solution {
 
 				match index {
 					0 => {
-						tail_visited.push(*rope_part);
+						tail_visited.set_true(*rope_part);
 					}
 					8 => {
-						long_tail_visited.push(*rope_part);
+						long_tail_visited.set_true(*rope_part);
 					}
 					_ => (),
 				}
@@ -117,13 +184,9 @@ pub fn solve(input: &[u8]) -> Solution {
 		}
 	}
 
-	let tail_visited_unique: HashSet<Point> = HashSet::from_iter(tail_visited.into_iter());
-	let long_tail_visited_unique: HashSet<Point> =
-		HashSet::from_iter(long_tail_visited.into_iter());
-
 	Solution(
-		tail_visited_unique.len() as u64,
-		long_tail_visited_unique.len() as u64,
+		tail_visited.count() as u64,
+		long_tail_visited.count() as u64,
 	)
 }
 
