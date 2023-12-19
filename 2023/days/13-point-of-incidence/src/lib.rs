9e353597a537d15625c270fcb5eac15fa2ac73af
Remmy Cat Stock <3317423+remmycat@users.noreply.github.com>
2023-12-19 10:12:19 +0100

    Faster mirrors

--- 2023/days/13-point-of-incidence/src/lib.rs
+++ 2023/days/13-point-of-incidence/src/lib.rs
@@ -1,159 +1,116 @@
 #[derive(Debug, PartialEq, Eq)]
 pub struct Solution(usize, usize);
 
-fn find_mirror_point(lines: &[&[u8]]) -> ((usize, usize), (usize, usize)) {
+fn find_mirror_point(lines: &[&[u8]]) -> (usize, usize) {
 	let width = lines[0].len();
 	let height = lines.len();
 	let dimension = width.max(height);
 
-	let mut candidates_vert: Vec<_> = (1..width).collect();
-	let mut candidates_horz: Vec<_> = (1..height).collect();
-
-	let mut candidates_vert_smudged: Vec<_> = (1..width).map(|c| (c, false)).collect();
-	let mut candidates_horz_smudged: Vec<_> = (1..height).map(|c| (c, false)).collect();
+	let mut candidates_vert: Vec<_> = (1..width).map(|c| (c, false)).collect();
+	let mut candidates_horz: Vec<_> = (1..height).map(|c| (c, false)).collect();
 
 	for index in 0..dimension {
+		let mut found_smudged_in_iteration = false;
+
 		if index < height {
 			let line = lines[index];
-			candidates_vert.retain(|candidate| {
-				line[0..*candidate]
+
+			candidates_vert.retain_mut(|(candidate, smudged)| {
+				let max_count = (*candidate).min(line.len() - *candidate);
+				let actual = line[0..*candidate]
 					.iter()
 					.rev()
 					.zip(line[*candidate..].iter())
-					.all(|(a, b)| a == b)
-			});
+					.filter(|(a, b)| a == b)
+					.count();
+
+				match max_count - actual {
+					0 => true,
+					// if we already have a smudge we cannot have another!
+					1 if !*smudged => {
+						found_smudged_in_iteration = true;
+						*smudged = true;
+						true
+					}
+					_ => false,
+				}
+			})
 		}
 
 		if index < width {
-			candidates_horz.retain(|candidate| {
-				lines[0..*candidate]
+			candidates_horz.retain_mut(|(candidate, smudged)| {
+				let max_count = (*candidate).min(lines.len() - *candidate);
+
+				let actual = lines[0..*candidate]
 					.iter()
 					.rev()
 					.zip(lines[*candidate..].iter())
-					.all(|(a, b)| a[index] == b[index])
-			})
-		}
+					.filter(|(a, b)| a[index] == b[index])
+					.count();
 
-		if candidates_horz.len() + candidates_vert.len() == 1 {
-			break;
-		}
-	}
-
-	if candidates_horz.len() + candidates_vert.len() != 1 {
-		panic!("expected candidates reduced to 1")
-	}
-
-	let unsmudged = if let Some(h_candidate) = candidates_horz.first() {
-		// remove unsmudged find from smudged candidates so we wont find it again
-		candidates_horz_smudged.retain(|(c, _)| *c != *h_candidate);
-		(0_usize, *h_candidate)
-	} else {
-		// remove unsmudged find from smudged candidates so we wont find it again
-		let v_candidate = candidates_vert.first().unwrap();
-		candidates_vert_smudged.retain(|(c, _)| *c != *v_candidate);
-		(*v_candidate, 0_usize)
-	};
-
-	for index in 0..dimension {
-		if index < height {
-			let line = lines[index];
-
-			candidates_vert_smudged.retain_mut(|(candidate, smudged)| {
-				if *smudged {
-					// already has a smudge cannot have another!
-					line[0..*candidate]
-						.iter()
-						.rev()
-						.zip(line[*candidate..].iter())
-						.all(|(a, b)| a == b)
-				} else {
-					let max_count = (*candidate).min(line.len() - *candidate);
-					let actual = line[0..*candidate]
-						.iter()
-						.rev()
-						.zip(line[*candidate..].iter())
-						.filter(|(a, b)| a == b)
-						.count();
-
-					match max_count - actual {
-						0 => true,
-						1 => {
-							*smudged = true;
-							true
-						}
-						_ => false,
+				match max_count - actual {
+					0 => true,
+					// if we already have a smudge we cannot have another!
+					1 if !*smudged => {
+						found_smudged_in_iteration = true;
+						*smudged = true;
+						true
 					}
+					_ => false,
 				}
 			})
 		}
 
-		if index < width {
-			candidates_horz_smudged.retain_mut(|(candidate, smudged)| {
-				if *smudged {
-					// already has a smudge cannot have another!
-					lines[0..*candidate]
-						.iter()
-						.rev()
-						.zip(lines[*candidate..].iter())
-						.all(|(a, b)| a[index] == b[index])
-				} else {
-					let max_count = (*candidate).min(lines.len() - *candidate);
-
-					let actual = lines[0..*candidate]
-						.iter()
-						.rev()
-						.zip(lines[*candidate..].iter())
-						.filter(|(a, b)| a[index] == b[index])
-						.count();
-
-					match max_count - actual {
-						0 => true,
-						1 => {
-							*smudged = true;
-							true
-						}
-						_ => false,
-					}
-				}
-			})
-		}
-
-		if candidates_horz_smudged.len() + candidates_vert_smudged.len() == 1 {
+		// While we will only have 2 candidates at some point (one for the smudged
+		// and one for the unsmudged set), we might not have found the smudge yet!
+		// We need to make sure we have found it, otherwise we don't know which of
+		// the two candidates should be counted for which part result.
+		if found_smudged_in_iteration && candidates_horz.len() + candidates_vert.len() == 2 {
 			break;
 		}
 	}
 
-	if candidates_horz_smudged.len() + candidates_vert_smudged.len() != 1 {
-		panic!("expected smudged candidates reduced to 1")
-	}
+	debug_assert_eq!(2, candidates_horz.len() + candidates_vert.len());
 
-	let smudged = candidates_horz_smudged
-		.first()
-		.map(|(c, _)| (0_usize, *c))
-		.or(candidates_vert_smudged.first().map(|(c, _)| (*c, 0_usize)))
-		.unwrap();
-
-	(unsmudged, smudged)
+	// horizontal is times a 100
+	candidates_horz
+		.into_iter()
+		.map(|(c, smudged)| {
+			if smudged {
+				(0_usize, c * 100)
+			} else {
+				(c * 100, 0)
+			}
+		})
+		.chain(candidates_vert.into_iter().map(
+			|(c, smudged)| {
+				if smudged {
+					(0_usize, c)
+				} else {
+					(c, 0)
+				}
+			},
+		))
+		.fold(
+			(0_usize, 0_usize),
+			|(acc_unsmudged, acc_smudged), (unsmudged, smudged)| {
+				(acc_unsmudged + unsmudged, acc_smudged + smudged)
+			},
+		)
 }
 
 pub fn solve(input: &[u8]) -> Solution {
 	let mut lines: Vec<&[u8]> = vec![];
 
-	let mut vertical_sum = 0;
-	let mut horizontal_sum = 0;
-
-	let mut vertical_sum_smudged = 0;
-	let mut horizontal_sum_smudged = 0;
+	let mut unsmudged_sum = 0;
+	let mut smudged_sum = 0;
 
 	for line in input.split(|b| *b == b'\n') {
 		if line.is_empty() {
 			// end of block;
-			let ((vert, horz), (vert_smudged, horz_smudged)) = find_mirror_point(&lines);
-			vertical_sum += vert;
-			horizontal_sum += horz;
-
-			vertical_sum_smudged += vert_smudged;
-			horizontal_sum_smudged += horz_smudged;
+			let (unsmudged, smudged) = find_mirror_point(&lines);
+			unsmudged_sum += unsmudged;
+			smudged_sum += smudged;
 
 			lines.clear();
 			continue;
@@ -162,11 +119,7 @@ pub fn solve(input: &[u8]) -> Solution {
 		lines.push(line);
 	}
 
-	let sum = horizontal_sum * 100 + vertical_sum;
-
-	let sum_smudged = horizontal_sum_smudged * 100 + vertical_sum_smudged;
-
-	Solution(sum, sum_smudged)
+	Solution(unsmudged_sum, smudged_sum)
 }
 
 #[cfg(test)]
